105c65051d4e958e6b303e56735cae5311fe0878
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-01-26 17:25:41 +0100

    basic config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { version } from '../../package.json';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'auto-release';
+  public version;
+  constructor() {
+    this.version = version;
+  }
 }
